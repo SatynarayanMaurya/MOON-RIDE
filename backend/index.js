14938c89b88bf9c34445c642660164f580669f8f
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2025-08-30 18:40:56 +0530

    Allowed multiple frontend urls

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,21 @@ import cors from "cors"
 const app = express();
 connectDb()
 
+const allowedOrigins = process.env.FRONTEND_URLS?.split(",");
+
 app.use(cors({
-  origin:process.env.FRONTEND_URL,
-  credentials:true
-}))
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
